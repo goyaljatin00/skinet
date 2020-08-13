dcad372c2717d9690c71336dff0ae5585c9cb57e
goyaljatin00 <62867300+goyaljatin00@users.noreply.github.com>
2020-08-13 15:36:18 +0530

    End of Section Home Page Decoration

--- Client/src/app/shop/product-details/product-details.component.ts
+++ Client/src/app/shop/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { ShopService } from '../shop.service';
 import { IProduct } from 'src/app/shared/models/product';
 import { ActivatedRoute } from '@angular/router';
 import { faMinusSquare, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -13,16 +14,21 @@ export class ProductDetailsComponent implements OnInit {
   faMinusSquare = faMinusSquare;
   faPlusSquare = faPlusSquare;
   product: IProduct;
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) 
+  {
+    this.bcService.set('@productDetails', '');
+  }
 
   ngOnInit(): void {
     this.loadProduct();
   }
 
+  // tslint:disable-next-line: typedef
   loadProduct()
   {
     this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe(product => {
       this.product = product;
+      this.bcService.set('@productDetails', product.name);
     }, error => {
       console.log(error);
     });
