75a5ce4e8022e626b9463d9c675bffb708355036
goyaljatin00 <62867300+goyaljatin00@users.noreply.github.com>
2020-08-24 17:52:08 +0530

    End of Secton 16

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string  Email { get; set; }
 
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", ErrorMessage
+        = "Password must have 1 uppercase, 1 lower case, 1 alphanumeric and 1 numneric")]
         public string Password { get; set; }
     }
 }
--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -11,6 +11,29 @@ namespace API.Extensions
             services.AddSwaggerGen(c => 
             {
                 c.SwaggerDoc("v1", new OpenApiInfo{Title = "SkiNet API", Version = "v1"});
+
+                var securtiySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securtiySchema);
+
+                var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    {securtiySchema, new [] {"Bearer"}}
+                };
+
+                c.AddSecurityRequirement(securityRequirement);
             });
 
             return services;
